34fcde70f3f6cb0fa1bcd58ef8e9d10d0d10d5b0
Yonas <yonas@mail.lan>
2024-02-16 07:06:02 -0500

    Update deps.

--- src/main.rs
+++ src/main.rs
@@ -325,19 +325,19 @@ fn read_tasks() -> Result<Vec<Group>> {
         dir = d.parent()
     }
 
-    // ~/.ttr.yaml
-    let home_dir_config = dirs::home_dir()
-        .map(|home| home.join(TTR_CONFIG))
-        .filter(|config| config.is_file());
-    if let Some(config) = home_dir_config {
-        tasks.push(tasks_from_file(config)?);
-    }
-
     // ~/.config/ttr/ttr.yaml
     let config_dir_config = dirs::config_dir()
         .map(|home| home.join("ttr").join("ttr.yaml"))
         .filter(|config| config.is_file());
     if let Some(config) = config_dir_config {
+        tasks.push(tasks_from_file(config)?);
+    }
+
+    // ~/.ttr.yaml
+    let home_dir_config = dirs::home_dir()
+        .map(|home| home.join(TTR_CONFIG))
+        .filter(|config| config.is_file());
+    if let Some(config) = home_dir_config {
         tasks.push(tasks_from_file(config)?);
     }
 
